21cea67cadf14b0948b993d74c904b3d1025c112
Dhafin Kawakibi <dhafinkawakibi@student.upi.edu>
2021-12-30 01:25:17 +0700

    try to using pointer

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	/*Try to declare string variable ( var {name_variable} {type_data} ) but golang is picky, it doesn't want to have a variable hanging around*/
 	var whatToSay string
 	var i int
+	var myString string
 	/*So I need to fill out the variable with some string*/
 	whatToSay = "Goodbye, cruel world"
 	/*looks like golang still being picky
@@ -22,8 +23,20 @@ func main() {
 	whatWasSaid, theOtherThingThatWhatSaid := saySomething()
 
 	fmt.Println("The function returned", whatWasSaid, theOtherThingThatWhatSaid)
+
+	myString = "Green"
+	fmt.Println("myString set to", myString)
+	changeUsingPointer(&myString) /*it's like replacing the variable you have in main to anything else in the function changeUsingPointer*/
+	fmt.Println("after func call myString is set to", myString)
 }
 
 func saySomething() (string, string) {
 	return "something", "else"
+}
+
+/*try using pointer*/
+
+func changeUsingPointer(s *string) {
+	newValue := "Red"
+	*s = newValue
 }
