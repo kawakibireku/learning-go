091714f781f616ae9879ab402608e4acdb445f8b
Dhafin Kawakibi <dhafinkawakibi@student.upi.edu>
2021-12-30 01:10:00 +0700

    try to declare variable integer

--- main.go
+++ main.go
@@ -8,10 +8,14 @@ func main() {
 
 	/*Try to declare string variable ( var {name_variable} {type_data} ) but golang is picky, it doesn't want to have a variable hanging around*/
 	var whatToSay string
+	var i int
 	/*So I need to fill out the variable with some string*/
 	whatToSay = "Goodbye, cruel world"
 	/*looks like golang still being picky
 	(it's still have the red underscore thingy on the whatToSay variable) because I still not using it,
 	so I used the fmt function to print line the variable */
 	fmt.Println(whatToSay)
+
+	i = 1
+	fmt.Println("i is set to", i)
 }
